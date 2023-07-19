da7b347a849c888421f980fa4306fbb7be12e185
Ramon <r.swilem@gravity.nl>
2023-07-19 14:41:49 +0200

    Update strapi-server.js

--- strapi-server.js
+++ strapi-server.js
@@ -4,6 +4,11 @@ const Permissions = require('./permissions');
 
 module.exports = {
   async bootstrap({ strapi }) {
+    // Disable permission setting when dumping or restoring configuration, or executing any other command.
+    if (!['start', 'develop'].includes(process.argv?.[2])) {
+      return;
+    }
+    
     const permissions = new Permissions(strapi);
     permissions.setup();
   }
